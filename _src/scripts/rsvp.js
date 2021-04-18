d9f835de43bfd7aecd4bffc99b3c19f576e57543
Michael Spoehr <michael.spoehr@dornerworks.com>
2021-04-18 18:17:39 -0400

    Improve form submit handling

--- _src/scripts/rsvp.js
+++ _src/scripts/rsvp.js
@@ -7,9 +7,14 @@ function setAttend(attend) {
 }
 
 function submitRsvp() {
+  const submitButton = document.getElementById('submit');
+  const form = document.getElementById('rsvp-form');
+  const status = document.getElementById('status');
+  const numAttendingField = document.getElementById('numAttending');
+
   const name = document.getElementById('name').value;
   const attending = document.getElementById('acceptInvite').checked;
-  const numAttending = document.getElementById('numAttending').value;
+  const numAttending = numAttendingField.value;
   const comments = document.getElementById('comments').value;
 
   if (!(name && attending !== undefined && numAttending && name.length > 0)) {
@@ -17,23 +22,31 @@ function submitRsvp() {
     return true;
   }
 
+  submitButton.disabled = true;
   const xhr = new XMLHttpRequest();
-  
+
+  const onError = () => {
+    submitButton.disabled = false;
+    status.innerHTML = "Something went wrong";
+    status.style.color = "red";
+  };
   xhr.addEventListener('load', (response) => {
-    const status = document.getElementById('status');
+    submitButton.disabled = false;
     if (response.target.status == 200 && response.target.response == "{}") {
       status.innerHTML = "Your RSVP was received. Thank you!";
       status.style.color = "#12795d";
+      form.reset();
+      numAttendingField.disabled = false;
     } else {
-      status.innerHTML = "Something went wrong";
-      status.style.color = "red";
+      onError();
     }
   });
+  xhr.addEventListener('error', onError);
 
   xhr.open('POST', 'https://54d6p8wjui.execute-api.us-east-2.amazonaws.com/Production/rsvp');
   xhr.setRequestHeader("Content-Type", "application/json");
   
-  const data = { 
+  const data = {
     name: name,
     attending: attending,
     numberAttending: Number(numAttending),
